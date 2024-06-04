9993a34199b78f5e9f6ce6188c5d95af97a0d506
James <50016206+jlancaster1223@users.noreply.github.com>
2024-06-04 22:56:08 +0100

    Create style.css

---
+++ _assets/style.css
@@ -0,0 +1,175 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<link rel="stylesheet" href="/_assets/style.css" />
+<nav>
+    <div class="header__topbar">
+        <div class="container">
+            <div class="header__topbar__left">
+                <div class="contrastToggle">
+                    <input type="checkbox" id="contrastToggle">
+                    <label for="contrastToggle">High Contrast</label>
+                </div>
+                <a href="#">Product Support</a>
+            </div>
+            <div class="header__topbar__right">
+                <div class="search">
+                    <i class="fa-solid fa-magnifying-glass"></i>
+                    <div>Search</div>
+                </div>
+                <a href="#">Contact Sales</a>
+                <a href="#">Our Story</a>
+                <a href="#">Login</a>
+            </div>
+        </div>
+    </div>
+    <header class="header__main">
+        <div class="container">
+        <div class="header__main__logo">
+            <img src="/_assets/logo.png" />
+        </div>
+        <nav class="header__main__menu">
+            <ul>
+                <li class="has-submenu">
+                    Products
+                    <div class="submenu submenu__style1">
+                        <div class="container">
+                            <div class="submenu__main">
+                                <div class="submenu__items">
+                                    <div class="submenu__items__single">
+                                        <img src="/_assets/icons/fork.png">
+                                        <div>
+                                            <h3>Fishtank</h3>
+                                            <p>Lorem ipsum dolor sit amet</p>
+                                        </div>
+                                    </div>
+                                    <div class="submenu__items__single">
+                                        <img src="/_assets/icons/fork.png">
+                                        <div>
+                                            <h3>Fishtank</h3>
+                                            <p>Lorem ipsum dolor sit amet</p>
+                                        </div>
+                                    </div>
+                                    <div class="submenu__items__single">
+                                        <img src="/_assets/icons/fork.png">
+                                        <div>
+                                            <h3>Fishtank</h3>
+                                            <p>Lorem ipsum dolor sit amet</p>
+                                        </div>
+                                    </div>
+                                    <div class="submenu__items__single">
+                                        <img src="/_assets/icons/fork.png">
+                                        <div>
+                                            <h3>Fishtank</h3>
+                                            <p>Lorem ipsum dolor sit amet</p>
+                                        </div>
+                                    </div>
+                                    <div class="submenu__items__single">
+                                        <img src="/_assets/icons/fork.png">
+                                        <div>
+                                            <h3>Fishtank</h3>
+                                            <p>Lorem ipsum dolor sit amet</p>
+                                        </div>
+                                    </div>
+                                    <div class="submenu__items__single">
+                                        <img src="/_assets/icons/fork.png">
+                                        <div>
+                                            <h3>Fishtank</h3>
+                                            <p>Lorem ipsum dolor sit amet</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="submenu__standoutimage">
+                                    <img src="/_assets/pexels-andrea-piacquadio-3801701.jpg">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="submenu__footer">
+                            <div class="container">
+                                <p>Need help getting started?</p>
+                                <a href="#" class="button button-outline">Contact Us</a>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li>Solutions</li>
+                <li class="has-submenu">
+                    Sectors
+                    <div class="submenu submenu__style2">
+                        <div class="container">
+                            <div class="submenu__sectors">
+                                <div class="submenu__style2__heading">Sectors</div>
+                                <div class="submenu__sectors__wrapper">
+                                    <a href="#" class="submenu__sectors__single">
+                                        <div class="submenu__sectors__single__image">
+                                            <img src="/_assets/pexels-andrea-piacquadio-3801701.jpg">
+                                        </div>
+                                        <h3>Foodservice</h3>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi luctus consectetur nisl, nec pulvinar sem aliquet ac. Duis efficitur fringilla rutrum. Vivamus accumsan nisi vitae erat malesuada, eu dapibus nunc tristique. Quisque pretium ultricies augue.</p>
+                                    </a>
+                                    <a href="#" class="submenu__sectors__single">
+                                        <div class="submenu__sectors__single__image">
+                                            <img src="/_assets/pexels-andrea-piacquadio-3801701.jpg">
+                                        </div>
+                                        <h3>Foodservice</h3>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi luctus consectetur nisl, nec pulvinar sem aliquet ac. Duis efficitur fringilla rutrum. Vivamus accumsan nisi vitae erat malesuada, eu dapibus nunc tristique. Quisque pretium ultricies augue.</p>
+                                    </a>
+                                    <a href="#" class="submenu__sectors__single">
+                                        <div class="submenu__sectors__single__image">
+                                            <img src="/_assets/pexels-andrea-piacquadio-3801701.jpg">
+                                        </div>
+                                        <h3>Foodservice</h3>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi luctus consectetur nisl, nec pulvinar sem aliquet ac. Duis efficitur fringilla rutrum. Vivamus accumsan nisi vitae erat malesuada, eu dapibus nunc tristique. Quisque pretium ultricies augue.</p>
+                                    </a>
+                                    <a href="#" class="submenu__sectors__single">
+                                        <div class="submenu__sectors__single__image">
+                                            <img src="/_assets/pexels-andrea-piacquadio-3801701.jpg">
+                                        </div>
+                                        <h3>Foodservice</h3>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi luctus consectetur nisl, nec pulvinar sem aliquet ac. Duis efficitur fringilla rutrum. Vivamus accumsan nisi vitae erat malesuada, eu dapibus nunc tristique. Quisque pretium ultricies augue.</p>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="submenu__links">
+                                <div class="submenu__style2__heading">By role</div>
+                                
+                                <ul>
+                                    <li>
+                                        <a href="#">Product Manager</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Head Chef</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Head of Operations</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li>Resources</li>
+                <li>Pricing</li>
+            </ul>
+        </nav>
+        <div class="header__main__actions">
+            <a href="#" class="button button-primary">Call to Action 1</a>
+            <a href="#" class="button button-secondary">Call to Action 1</a>
+        </div>
+        </div>
+    </header>
+    <div class="header__usps">
+        <div class="container">
+            <span class="clearfix"></span>
+            <div class="header__usps__content">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                <a href="#">Read article</a>
+            </div>
+            <span class="header__usps__close" onclick="closeUSPs()">
+                <i class="fa-solid fa-xmark"></i>
+            </span>
+        </div>
+    </div>
+</nav>
+
+<!-- Include jquery -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="/_assets/script.js"></script>
